b35287731632eb909a624b9b18d3a52a09947969
fers9021 <fercan.90@gmail.com>
2024-04-21 20:26:14 +0200

    sun commit

--- Lesson14/fercan/script.js
+++ Lesson14/fercan/script.js
@@ -1,6 +1,6 @@
 // 9. Declare a variable number6 and initialize it to 6. Declare a variable number7 and initialize it to `++number6` (number6 incremented using prexif ++ operator). Log both values to the console.
-const number6 = 6 ;
-const number7 = '++number6' ;
+let number6 = 6 ;
+let number7 = '++number6' ;
    
 
 console.log('Exercise 9:, ${number7}');
@@ -8,14 +8,14 @@ console.log('------------------------');
 
 
 // 10. Declare a variable number8 and initialize it to 8. Declare a variable number9 and initialize it to `number8++` (number8 incremented using postfix ++ operator). Log both values to the console.
-const number8 = 8;
+let number8 = 8;
 let number9 = number8++
 
 console.log('number8, number9');
-fffff
+
 
 // 11. Declare a variable number10 and initialize it to -3. Use the postfix ++ operator to increment the value of num by 1, then multiply the result by 2 and log it to the console.
-const number10 = -3;
+let number10 = -3;
 number10 +=
 console.log('------------------------');
 
