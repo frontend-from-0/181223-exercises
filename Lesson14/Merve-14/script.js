a23caca3633e1ac7fad89e0333ea4c25784bca16
MerveAyakdaş <m.z.ayakdas@gmail.com>
2024-04-24 12:22:45 +0300

    L-14 Merve(Last changes)

---
+++ Lesson14/Merve-14/script.js
@@ -0,0 +1,22 @@
+// 9. Declare a variable number6 and initialize it to 6. Declare a variable number7 and initialize it to `++number6` (number6 incremented using prexif ++ operator). Log both values to the console.
+const number6 = 6;
+const number7 = ++number6;
+console.log("number6:", number6, "number7:", number7);
+console.log('------------------------');
+// 10. Declare a variable number8 and initialize it to 8. Declare a variable number9 and initialize it to `number8++` (number8 incremented using postfix ++ operator). Log both values to the console.
+const number8 = 8;
+const number9 = number8++;
+console.log("number8:", number8, "number9:", number9);
+console.log('------------------------');
+// 11. Declare a variable number10 and initialize it to -3. Use the postfix ++ operator to increment the value of num by 1, then multiply the result by 2 and log it to the console.
+const number10 = -3;
+number10++;
+const result10 = number10 * 2;
+console.log("Result:", result10);
+console.log('------------------------');
+// 12. Declare two variables a and b and initialize them to 2 and 3, respectively. Use the prefix ++ operator to increment the value of a by 1, then add b to the result and log it to the console.
+const a = 2;
+const b = 3;
+const result = ++a + b;
+console.log("Result:", result2);
+console.log('------------------------');
