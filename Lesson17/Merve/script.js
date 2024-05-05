8d81c922471ac4b7c9b5f91201b0ddb64f3607c7
MerveAyakdaş <m.z.ayakdas@gmail.com>
2024-05-05 17:26:19 +0300

    L-17 Merve Corrections

--- Lesson17/Merve/script.js
+++ Lesson17/Merve/script.js
@@ -10,22 +10,28 @@ for (let i = 1; i <= 20; i++) {
 console.log('- - - - - - - - - - - - - - - - - ');
 // 3. Write a loop that prints out the even numbers from 1 to 20.
 // 2, 4, 6...
-for (let i = 1; i <= 20; i++) {
+for (let i = 2; i <= 20; i += 2) {
     console.log(i);
 }
 console.log('- - - - - - - - - - - - - - - - - ');
 // 4. Write a loop that finds the maximum number in an array of numbers.
-const findMaxNumberInArray = [300, 1000, 23, 42, 300, 120, 7, 4, 90];
-for (let i = 0; i < findMaxNumberInArray.length; i++) {
-    console.log(findMaxNumberInArray[i]);
+const findMaxNumberInArray = [300, 1000, 23, 42, 300, 12, 7, 4, 90];
+let maxNumber = findMaxNumberInArray[0];
+for (let i = 1; i < findMaxNumberInArray.length; i++) {
+	if (findMaxNumberInArray[i] > maxNumber) {
+		maxNumber = findMaxNumberInArray[i];
+	}
 }
-console.log('- - - - - - - - - - - - - - - - - ');
+console.log(maxNumber);
 // 5. Write a loop that finds the minimum number in an array of numbers.
 const findMinNumberInArray = [1, 10, 23, 42, 0, 12, 7, 0];
-for (let i = 0; i < findMinNumberInArray.length; i++) {
-    console.log(findMinNumberInArray[i]);
+let minNumber = findMinNumberInArray[0];
+for (let i = 1; i < findMinNumberInArray.length; i++) {
+	if (findMinNumberInArray[i] < minNumber) {
+		minNumber <= findMinNumberInArray[i];
+	}
 }
-console.log('- - - - - - - - - - - - - - - - - ');
+console.log(minNumber);
 // 6. Write a loop that reverses a given string.
 const string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit.';
 let reversedString = '';
@@ -62,19 +68,23 @@ for (const key in person) {
   }
 // 11. Write a for...in loop that sums the values of an object.
 const obj = { a: 10, b: 20, c: 30, d: 23 };
-for (const key in obj) {
-    console.log(`${key}: ${obj[key]}`);
-  }
+var sum = 0;  //first i did use "let" but cursor keep giving warnings like : Cannot redeclare block-scoped variable 'sum'. \\
+for (let key in obj) {
+  sum += obj[key];
+}
+console.log(sum);
 // Excercises below are additional (as requested during the lesson)
 
 // 12. Sum of Random Numbers: Write a do-while loop that generates random numbers between 1 and 5 and adds them together until the sum is greater than 20. Print the total number of iterations and the final sum.
+let sum = 0;
+let evenCount = 0;
 do {
-    let sum = 0;
-    for (let i = 0; i < 5; i++) {
-        sum += Math.floor(Math.random() * 5) + 1;
-    }
+    sum += Math.floor(Math.random() * 5) + 1;
+    evenCount++;
     console.log(sum);
 } while (sum <= 20);
+
+console.log(`Total even numbers: ${evenCount}`);
 // 13. Count Even Numbers: Write a for loop that generates and checks 100 random numbers between 1 and 50. Count how many even numbers are generated and print the count at the end.
 for (let i = 0; i < 100; i++) {
     if (i % 2 === 0) {
@@ -117,8 +127,10 @@ while (true) {
     }
 }
 // 16. Array Element Doubling: Write a for loop that iterates over an array of numbers (e.g., [1, 2, 3, 4, 5]). For each iteration, double the value of each element and store the results in a new array. Print the original and the new array at the end.
+const numbers = [1, 2, 3, 4, 5];
 for (let i = 0; i < numbers.length; i++) {
     numbers[i] *= 2;
+    let doubleNumbers = [];
 }
 console.log(numbers);
 
