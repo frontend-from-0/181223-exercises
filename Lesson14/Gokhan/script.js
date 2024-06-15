404855008e862a34d2e4bb893d2bea9e43502495
G-Memis <gokhanmemis10@gmail.com>
2024-06-16 02:51:44 +0300

    İ did it again

--- Lesson14/Gokhan/script.js
+++ Lesson14/Gokhan/script.js
@@ -1,12 +1,43 @@
-// 9. Declare a variable number6 and initialize it to 6. Declare a variable number7 and initialize it to `++number6` (number6 incremented using prexif ++ operator). Log both values to the console.
+// 9. Declare a variable number6 and initialize it to 6. 
+// Declare a variable number7 and initialize it to `++number6` 
+// (number6 incremented using prexif ++ operator). Log both values to the console.
+
+let number6 = 6;
+const number7 = ++number6;
+
+
+console.log(number6, number7);
+
+
+
+// 10. Declare a variable number8 and initialize it to 8. 
+// Declare a variable number9 and initialize it to `number8++` 
+// (number8 incremented using postfix ++ operator). Log both values to the console.
+
+let number8 = 8;
+const number9 = number8++;
+
+
+console.log(number8, number9);
+
+
+
+// 11. Declare a variable number10 and initialize it to -3. 
+// Use the postfix ++ operator to increment the value of num by 1, 
+// then multiply the result by 2 and log it to the console.
+
+
+
 
 console.log('------------------------');
-// 10. Declare a variable number8 and initialize it to 8. Declare a variable number9 and initialize it to `number8++` (number8 incremented using postfix ++ operator). Log both values to the console.
 
-console.log('------------------------');
-// 11. Declare a variable number10 and initialize it to -3. Use the postfix ++ operator to increment the value of num by 1, then multiply the result by 2 and log it to the console.
 
-console.log('------------------------');
-// 12. Declare two variables a and b and initialize them to 2 and 3, respectively. Use the prefix ++ operator to increment the value of a by 1, then add b to the result and log it to the console.
+
+// 12. Declare two variables a and b and initialize them to 2 and 3, 
+// respectively. Use the prefix ++ operator to increment the value of a by 1, 
+// then add b to the result and log it to the console.
+
+
+
 
 console.log('------------------------');
