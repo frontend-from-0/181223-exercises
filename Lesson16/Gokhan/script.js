176cf55623c4760bd3c8a3973c321a3ce1dd9849
Gökhan MEMİŞ <154741383+G-Memis@users.noreply.github.com>
2024-06-30 02:01:56 +0300

    Update Lesson16/Gokhan/script.js
    Co-authored-by: Anna S. Atalay <anna.atalay@minna.tech>

--- Lesson16/Gokhan/script.js
+++ Lesson16/Gokhan/script.js
@@ -138,7 +138,7 @@ function checkVotingEligibility(age, isStudent) {
 checkVotingEligibility(16, true);
 checkVotingEligibility(40, false);
 checkVotingEligibility(65, false);
-checkVotingEligibility(22, "yes");
+checkVotingEligibility(22, true);
 
 // 11. Write a function that asks the user to enter their salary and their years of experience. 
 //     If the salary is less than $30,000 and the years of experience are less than 5, 
