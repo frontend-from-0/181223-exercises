8d67c6622c6977e1544fd4a7d7ac9207db05a5eb
fers9021 <fercan.90@gmail.com>
2024-07-22 16:16:31 +0200

    second commit-file setup

--- Lesson25/L25-fercan/filter.js
+++ Lesson25/L25-fercan/filter.js
@@ -2,11 +2,6 @@
 // Given an array of numbers, filter out the even numbers.
 const numbersExercise1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const oddNumbers = numbersExercise1.filter(number => number % 2 !== 0);
-
-console.log(oddNumbers); 
-
-
 // Exercise 2:
 // Given an array of strings, filter out the strings that contain the letter "a".
 const wordsExercise2 = [
