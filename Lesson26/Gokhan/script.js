57fd876bc7aa247e6d14b0ffb18a47a734126994
G-Memis <gokhanmemis10@gmail.com>
2024-08-04 16:00:17 +0300

    Add Reset Button for Lesson 26

--- Lesson26/Gokhan/script.js
+++ Lesson26/Gokhan/script.js
@@ -1,5 +1,5 @@
 // replace with your own API KEY (register on unsplash.com to get it).
-const API_KEY = 'qCnxm3oi9NS9mzAkGmh7_4B6V3p6DJnyFs_wKfNQFF4';
+const API_KEY = 'UcxmetuPnt2wd3vltsdXqYaOSvDxirnsB7rh5iRrBHI';
 
 const button = document.getElementById('fetch-button');
 button.addEventListener('click', () => {
@@ -17,3 +17,13 @@ button.addEventListener('click', () => {
 		)
 		.catch((error) => console.log(error));
 });
+
+const resetButton = document.getElementById('reset-button');
+resetButton.addEventListener('click', function () {
+	location.reload();
+});
+
+
+
+// Lesson 26 My API_KEYUcxmetuPnt2wd3vltsdXqYaOSvDxirnsB7rh5iRrBHI
+// Lesson 26 API_KEY qCnxm3oi9NS9mzAkGmh7_4B6V3p6DJnyFs_wKfNQFF4 Lesson 26 API_KEY
