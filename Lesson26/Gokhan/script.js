d89b5a50d5283e8ced1b950589e366e2ef6c6883
G-Memis <gokhanmemis10@gmail.com>
2024-09-03 23:43:52 +0300

    All Comments Fixed

--- Lesson26/Gokhan/script.js
+++ Lesson26/Gokhan/script.js
@@ -40,31 +40,3 @@ resetButton.addEventListener('click', () => {
   imageContainer.innerHTML = '';
   resetButton.classList.add('hidden');
 });
-
-// const resetButton = document.getElementById('reset-button');
-// resetButton.style.display = 'none';
-
-// fetchButton.addEventListener('click', () => {
-// 	resetButton.style.display = 'block';
-// 	resetButton.style.margin = '0 auto';
-// });
-
-// resetButton.addEventListener('click', () => {
-// 	imageContainer.innerHTML = '';
-// 	resetButton.style.display = 'none';
-// });
-
-// Reset Button for Clear The Images
-// document.addEventListener('DOMContentLoaded', function () {
-// 	const fetchButton = document.getElementById('fetch-button');
-// 	const resetButton = document.querySelector('button[type="reset"]');
-// 	const imageContainer = document.getElementById('image-container');
-
-// 	fetchButton.addEventListener('click', function () {
-// 		resetButton.style.display = 'none';
-// 	});
-
-// 	imageContainer.addEventListener('mouseover', function () {
-// 		resetButton.style.display = 'block';
-// 	});
-// });
