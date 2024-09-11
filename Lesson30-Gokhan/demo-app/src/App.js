935e233c25923b11ad77a4a191fdd93ddaf452aa
Gökhan MEMİŞ <154741383+G-Memis@users.noreply.github.com>
2024-09-11 09:48:15 +0300

    Lesson 30 Gokhan (#121)
    * Extra Assignment
    
    * i added my branch to main
    
    * Lesson 26 PR
    
    * Deleted My Wrong Branch
    
    * Lesson 30 Branch
    
    * Lesson 30 React App
    
    * Lesson 30 Branch
    
    * Updated App.js(x)

---
+++ Lesson30-Gokhan/demo-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Wait for me at the dawn of the 5th day. Look east at dawn.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React With Gokhan
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
