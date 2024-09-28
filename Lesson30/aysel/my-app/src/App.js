c3df6b75c0850aa3c0198fb4a1d541d640c7d67f
Aysel Akpinar <ayselesya96@gmail.com>
2024-09-28 22:55:24 +0300

    lesson30-aysel

---
+++ Lesson30/aysel/my-app/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+export function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Welcome to my very first React app!</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
