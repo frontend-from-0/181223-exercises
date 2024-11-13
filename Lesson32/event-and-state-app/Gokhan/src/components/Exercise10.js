f6a31054a1109120d096016a1b685579bac9ce3e
G-Memis <gokhanmemis10@gmail.com>
2024-11-13 10:37:47 +0300

    All Conversations Resolved

--- Lesson32/event-and-state-app/Gokhan/src/components/Exercise10.js
+++ Lesson32/event-and-state-app/Gokhan/src/components/Exercise10.js
@@ -1,19 +1,11 @@
-import { useEffect } from 'react';
-
 export const Exercise10 = () => {
 
     const handleDoubleClick = () => {
         console.log('The Button Was Double Clicked');
     };
 
-    useEffect(() => {
-
-    }, []);
-
     return (
-        <>
-            <button onDoubleClick={handleDoubleClick}>Please Double Click The Button</button>
-        </>
+        <button onDoubleClick={handleDoubleClick}>Please Double Click The Button</button>
     );
 
 };
