2afc2eb3f6bb84e36bd845879e139b84946db2bf
mervebngl <121290654+mervebngl@users.noreply.github.com>
2024-12-06 00:24:09 +0300

    lesson33-merve2 (#137)

---
+++ Lesson33/merve2/src/components/Navbar/index.jsx
@@ -0,0 +1,15 @@
+import './styles.css';
+
+export const Navbar = ({ isLoggedInUser, updateUser }) => {
+    return (
+        <nav className="navbar">
+            {isLoggedInUser && (
+                <>
+                    <a href="#home">Home</a>
+                    <a href="#account">Account</a>
+                    <a href="#signout" onClick={() => updateUser({ isLoggedInUser: false })}>Sign out</a>
+                </>
+            )}
+        </nav>
+    );
+};
