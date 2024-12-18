e7c21b4eee2a206327880b711b28b6232e0da04f
G-Memis <gokhanmemis10@gmail.com>
2024-12-18 23:01:21 +0300

    1

--- Lesson33/Gokhan/src/components/Account/index.jsx
+++ Lesson33/Gokhan/src/components/Account/index.jsx
@@ -1,11 +1,24 @@
-import { UserDetailsForm } from "../UserDetailsForm";
+import { UserDetailsForm } from '../UserDetailsForm';
+import './styles.css';
 
-export const Account = () => {
-
+export const Account = ({ incomingUsername, updateUsername }) => {
   return (
     <div className="account-container">
       <h1>Account information</h1>
-      <UserDetailsForm />
+      <UserDetailsForm incomingUsername={incomingUsername} updateUsername={updateUsername} />
     </div>
   );
-};
+}
+
+
+// import { UserDetailsForm } from "../UserDetailsForm";
+
+// export const Account = () => {
+
+//   return (
+//     <div className="account-container">
+//       <h1>Account information</h1>
+//       <UserDetailsForm />
+//     </div>
+//   );
+// };
