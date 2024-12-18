935bf7a7c292a755d0583fa97165e1f96d09d4b2
G-Memis <gokhanmemis10@gmail.com>
2024-12-18 23:10:20 +0300

    3

--- Lesson33/Gokhan/src/components/PerformanceState/index.jsx
+++ Lesson33/Gokhan/src/components/PerformanceState/index.jsx
@@ -1,3 +1,12 @@
-export const PerformanceState = ({ completedTodos, totalTodos }) => {
-	return <div>{`Completed ${completedTodos} of ${totalTodos}`}</div>;
+
+import './styles.css';
+
+export const PerformanceState = ({ totalTodos, completedTodos }) => {
+	return (
+		<div className="performance-state">
+			<h2>Performance State</h2>
+			<p>Total Todos: {totalTodos}</p>
+			<p>Completed Todos: {completedTodos}</p>
+		</div>
+	);
 };
