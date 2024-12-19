3ed9fdb2fa811f3efe2c9cdf886f791c0042c80e
G-Memis <gokhanmemis10@gmail.com>
2024-12-19 17:00:39 +0300

    where is Navbar? :)

--- Lesson33/Gokhan/src/components/Navbar/index.jsx
+++ Lesson33/Gokhan/src/components/Navbar/index.jsx
@@ -7,7 +7,7 @@ export const Navbar = ({ isLoggedInUser, updateUser }) => {
 				<>
 					<a href="#home">Home</a>
 					<a href="#account">Account</a>
-					<a href="#signout" onClick={() => updateUser({ isLoggedInUser: false })}>Sign out</a>
+					<a href="#signout" onClick={() => updateUser({ isLoggedInUser: true })}>Sign out</a>
 				</>
 			)}
 		</nav>
